159beb9a84d935efabd1f9afc758037f9d1d5cad
gdelvecchiodelvecchio <giuseppedelvecchiodelvecchio@gmail.com>
2024-10-22 16:19:25 +0200

    update glow

--- mouse_glow.js
+++ mouse_glow.js
@@ -4,6 +4,6 @@ document.addEventListener("mousemove", function(e) {
       const mouseY = e.clientY;
   
       // Set the radial gradient's position based on mouse coordinates
-      glow.style.background = `radial-gradient(circle at ${mouseX}px ${mouseY}px, rgba(96, 92, 139, 0.35), rgba(21, 21, 42, 0.0) 20%)`;
+      glow.style.background = `radial-gradient(circle at ${mouseX}px ${mouseY}px, rgba(96, 92, 139, 0.35), rgba(21, 21, 42, 0.0) 40%)`;
   });
   
