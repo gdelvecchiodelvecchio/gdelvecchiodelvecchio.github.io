e85aa55c1488e5cd005c18a5e03c6c32f91d849b
gdelvecchiodelvecchio <giuseppedelvecchiodelvecchio@gmail.com>
2024-10-24 16:28:07 +0200

    fixed mouse glow effect at initial load

---
+++ hi_button.js
@@ -0,0 +1,7 @@
+// hi button listener
+
+function f() {
+      const button = document.getElementById("HiButton")
+}
+
+document.addEventListener("click", f)
--- mouse_glow.js
+++ mouse_glow.js
@@ -1,9 +1,20 @@
+
+// first I get the position of the mouse when the page loads and activate the effect only when the mouse moves
+
+
 document.addEventListener("mousemove", function(e) {
-      const glow = document.getElementById("glow-effect");
-      const mouseX = e.clientX;
-      const mouseY = e.clientY;
+  const glow = document.getElementById("glow-effect");
+  glow.style.display = 'block';
+});
+
+document.addEventListener("mousemove", function(e) {
+    const glow = document.getElementById("glow-effect");
+    const mouseX = e.pageX;
+    const mouseY = e.pageY;
+    // Set the radial gradient's position based on mouse coordinates
+    glow.style.background = `radial-gradient(circle at ${mouseX}px ${mouseY}px, rgba(96, 92, 139, 0.35), rgba(21, 21, 42, 0.0) 40%)`;
+});
+
   
-      // Set the radial gradient's position based on mouse coordinates
-      glow.style.background = `radial-gradient(circle at ${mouseX}px ${mouseY}px, rgba(96, 92, 139, 0.35), rgba(21, 21, 42, 0.0) 40%)`;
-  });
-  
+//glow-effect
+
